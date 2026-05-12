f492eb3b2e4008001b63d9ed4f8285a0bd122d26
sgrinley <shamar808@yahoo.com>
2026-05-12 12:46:15 -0700

    feat: Add Instance Variables and extends to inherit from the parent class

--- src/main/java/com/pluralsight/House.java
+++ src/main/java/com/pluralsight/House.java
@@ -1,4 +1,10 @@
 package com.pluralsight;
 
-public class House {
+public class House extends Asset {
+
+    //    Add Data Fields
+    private String address;
+    private int condition;
+    private int squareFoot;
+    private int lotSize;
 }
