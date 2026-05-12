52de5de64383cc896a3c3e51c97212c2de236794
sgrinley <shamar808@yahoo.com>
2026-05-12 13:02:01 -0700

    feat(House): Generate Constructor method & add address, condition, and size fields to constructor

--- src/main/java/com/pluralsight/House.java
+++ src/main/java/com/pluralsight/House.java
@@ -7,4 +7,16 @@ public class House extends Asset {
     private int condition;
     private int squareFoot;
     private int lotSize;
+
+    //    Generate Constructor & Add Properties
+    public House(String description, String dateAcquired, double originalCost) {
+        super(description, dateAcquired, originalCost);
+
+        this.address = address;
+        this.condition = condition;
+        this.squareFoot = squareFoot;
+        this.lotSize = lotSize;
+    }
+
+
 }
