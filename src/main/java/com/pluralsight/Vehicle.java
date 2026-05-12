68599072b8ca880a726b73b6d70c7590c789fbfd
sgrinley <shamar808@yahoo.com>
2026-05-12 14:26:37 -0700

    feat(House): Generate Constructor

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -7,4 +7,12 @@ public class Vehicle extends Asset {
     private int year;
     private int odometer;
 
+    //   Generate Constructor
+    public Vehicle(String description, String dateAcquired, double originalCost, String makeModel, int year, int odometer) {
+        super(description, dateAcquired, originalCost);
+        this.makeModel = makeModel;
+        this.year = year;
+        this.odometer = odometer;
+    }
+
 }
